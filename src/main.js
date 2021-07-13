d63bec4df292b1676126a1828af5e579531bfea6
osman <osmntamer@gmail.com>
2021-07-13 22:32:54 +0300

    .

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import store from './store'
 import vuetify from './plugins/vuetify'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
 new Vue({
   store,
